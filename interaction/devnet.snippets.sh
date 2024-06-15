1c38f3515c36eb60ae69cda22fce27648fb40561
Mihai Daniel Eremia <22687907+mihaieremia@users.noreply.github.com>
2024-06-16 02:02:24 +0400

    verifyContract

--- interaction/devnet.snippets.sh
+++ interaction/devnet.snippets.sh
@@ -6,7 +6,7 @@ LIQUID_SC=erd1qqqqqqqqqqqqqpgq04vxf48vdlr97p3jz73qtxlf4l9p8rezah0s37nzrm
 AGGREGATOR_SC=erd1qqqqqqqqqqqqqpgqh96hhj42huhe47j3jerlec7ndhw75gy72gesy7w2d6
 BURN_RATE=3000
 SHARE_RATE=4000
-PROJECT="/Users/mihaieremia/GitHub/rs-accumulator/output/accumulator.wasm"
+PROJECT="./output-docker/accumulator/accumulator.wasm"
 
 deploy() {
     mxpy contract deploy --bytecode=${PROJECT} --recall-nonce \
@@ -24,4 +24,10 @@ upgrade() {
     --arguments ${LIQUID_SC} ${BURN_RATE} ${SHARE_RATE} ${TOKEN} ${LIQUID_TOKEN} ${AGGREGATOR_SC} \
     --ledger --ledger-account-index=0 --ledger-address-index=0 \
     --gas-limit=150000000 --send --proxy=${PROXY} --chain=D || return
+}
+
+verifyContract() {
+    mxpy --verbose contract verify "${ADDRESS}"  \
+    --packaged-src=./output-docker/accumulator/accumulator-0.0.0.source.json --verifier-url="https://devnet-play-api.multiversx.com" \
+    --docker-image="multiversx/sdk-rust-contract-builder:v8.0.0" --ledger --ledger-account-index=0 --ledger-address-index=0  || return 
 }
